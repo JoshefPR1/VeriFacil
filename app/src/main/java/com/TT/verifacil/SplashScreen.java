88245511de5c0766e43758be7a771b9b3c680b72
Joshef <joshef.pacheco@gmail.com>
2021-04-13 14:45:47 -0500

    Versión 1.1 Se agrego S_Screen

---
+++ app/src/main/java/com/TT/verifacil/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.TT.verifacil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import java.time.Duration;
+import java.util.Set;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int DURATION_SPLASH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_BEHIND);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURATION_SPLASH);
+    }
+}
